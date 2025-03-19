83a73c722de6f7eb76880e2b8bc7432624948c9a
socra167 <socra167@gmail.com>
2025-03-19 12:42:59 +0900

    Kotlin for until

--- src/main/kotlin/exam3/Exam3.kt
+++ src/main/kotlin/exam3/Exam3.kt
@@ -2,4 +2,10 @@ fun main() {
     for (i in 0..4) {
         println("Count: $i")
     }
+
+    // until은 앞의 수는 포함하되, 뒷 수는 포함하지 않는다
+    // 배열 탐색에 자주 사용된다
+    for (i in 1 until 100) {
+        println(i)
+    }
 }

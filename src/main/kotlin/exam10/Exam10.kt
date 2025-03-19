f902c70b149439d448ad6a36eaca33817b7489fc
socra167 <socra167@gmail.com>
2025-03-19 15:24:34 +0900

    note

--- src/main/kotlin/exam10/Exam10.kt
+++ src/main/kotlin/exam10/Exam10.kt
@@ -10,6 +10,6 @@ fun main() {
     val p1 = Person("Alice", 29)
     val p2 = Person("Alice", 29)
 
-    println(p1)
-    println(p1 == p2)
+    println(p1) // 자동 생성된 toString() 메서드 사용
+    println(p1 == p2) // 자동 생성된 equals() 메서드 사용, ==로 사용해도 된다
 }

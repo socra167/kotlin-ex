62b18fc5c114d16977aee995b57cf23f2590b9b9
socra167 <socra167@gmail.com>
2025-03-19 12:50:35 +0900

    Kotlin fun

---
+++ src/main/kotlin/exam4/Exam4.java
@@ -0,0 +1,12 @@
+package exam4;
+
+public class Exam4 {
+	public static void main(String[] args) {
+		int result = add(5, 10);
+		System.out.println("Result: " + result);
+	}
+
+	public static int add(int a, int b) {
+		return a + b;
+	}
+}
---
+++ src/main/kotlin/exam4/Exam4.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val result = add(5, 10)
+    println("Result: $result")
+}
+
+fun add(a: Int, b: Int): Int {
+    return a + b
+}

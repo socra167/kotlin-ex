16e997e17c769f38176a628d86a18924ea90dc94
socra167 <socra167@gmail.com>
2025-03-21 09:01:44 +0900

    Kotin string template

---
+++ src/main/kotlin/exam14/Exam14.java
@@ -0,0 +1,11 @@
+package exam14;
+
+public class Exam14 {
+
+	public static void main(String[] args) {
+		String name = "Alice";
+		int age = 25;
+
+		System.out.println("My name is " + name + " and I am " + age + " years old.");
+	}
+}
